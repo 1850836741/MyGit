c6acc4d74509ebecccefdd8d185124f5acac453c
1850836741 <1850836741@qq.com>
2020-01-12 22:45:39 +0800

    eduction_platform beginning

---
+++ eduction_platform/src/main/java/com/example/eduction_platform/EductionPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.eduction_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EductionPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EductionPlatformApplication.class, args);
+    }
+
+}
